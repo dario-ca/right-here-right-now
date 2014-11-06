5af1881f1a7879267788876acb18ec4f60947065
Dario Casula <dario.bs91@gmail.com>
2014-11-06 00:30:16 -0600

    colors

--- app/js/color/colors.js
+++ app/js/color/colors.js
@@ -5,3 +5,9 @@ Colors.station = {
     DIVVY_STATION_FULL: "#0B577A",
     DIVVY_STATION_EMPTY: "#BABABA"
 };
+
+Colors.pothole = {
+  POTHOLE_OPEN: "#3F8643",
+  POTHOLE_OPEN_DUP: "yellow",
+  POTHOLE_COMPLETED: "#768A78"
+};
