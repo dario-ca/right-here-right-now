a48e2d17de8cfc3eeaf16f04c443a05b28339d79
paduano <auerbach98>
2014-11-18 16:54:40 -0600

    horizontal bar view

--- app/js/viewController/graph/noSelectionGraphViewController.js
+++ app/js/viewController/graph/noSelectionGraphViewController.js
@@ -6,13 +6,21 @@ var NoSelectionGraphViewController = function(layer, sublayer) {
                         {text:"SELECTION", color:Colors.graph.SELECTION}]);
 
 
-        var verticalBar = VerticalBarView([100,10,30,80,30,60,80,30],
-            ["Prova", "Bau", "Ciao", "XXXXX"],
-            [Colors.graph.CHICAGO, Colors.graph.SELECTION],
-            "crimes / population"
-        );
-        self.view.append(verticalBar);
+        if(sublayer == "Property Crime"){
+            var verticalBar = HorizontalBarView([100,10,30,80],
+                ["Prova", "Bau"],
+                [Colors.graph.CHICAGO, Colors.graph.SELECTION],
+                "crimes / population"
+            );
+        } else {
+            var verticalBar = VerticalBarView([100,10,30,80,30,60,80,30],
+                ["Prova", "Bau", "Ciao", "XXXXX"],
+                [Colors.graph.CHICAGO, Colors.graph.SELECTION],
+                "crimes / population"
+            );
+        }
 
+        self.view.append(verticalBar);
     }();
 
     return self;
